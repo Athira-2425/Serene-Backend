2ac288583c2cd9cea1cb2c0453fe70ce53df1cf3
Athira-2425 <athirashylesh19@gmail.com>
2025-11-27 23:56:10 +0530

    Delete frontend directory

--- frontend/Talk.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GenericPageLayout from '../components/GenericPageLayout';
-
-const Talk = () => {
-  return <GenericPageLayout title="Talk" subtitle="Find calm and focus with our guided sessions." />;
-};
-
-export default Talk;
